d25737dc0286ad36962d7394defe4ac46f0e6365
Johnathan Babb <johnathanwbabb@gmail.com>
2025-09-21 19:12:43 -0600

    updated the file

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340database_di10_user;
+    OWNER TO database502;
 
 
     -- Table structure for table `classification`
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS public.inventory
     inv_image character varying NOT NULL,
     inv_thumbnail character varying NOT NULL,
     inv_price numeric(9,0) NOT NULL,
-    inv_miles intger NOT NULL, 
+    inv_miles integer NOT NULL, 
     inv_color character varying NOT NULL,
     classification_id integer NOT NULL,
     CONSTRAINT inventory_pkey PRIMARY KEY (inv_id)
