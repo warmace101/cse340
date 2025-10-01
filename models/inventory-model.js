3eec2c1552496f59cabbb5f56d63f79d5d858304
Johnathan Babb <johnathanwbabb@gmail.com>
2025-09-30 21:19:16 -0600

    fixing mistakes in assignment 3

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -18,7 +18,7 @@ async function getInventoryByClassificationId(classification_id) {
       `SELECT * FROM public.inventory AS i 
       JOIN public.classification AS c 
       ON i.classification_id = c.classification_id 
-      WHERE i.classification_id = 1`,
+      WHERE i.classification_id = $1`,
       [classification_id]
     )
     return data.rows
