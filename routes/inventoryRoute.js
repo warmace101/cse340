614e8739f3df4a8ff89447337a095ac1567c0ef0
Johnathan Babb <johnathanwbabb@gmail.com>
2025-09-30 19:45:35 -0600

    week 3 test

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,7 +2,11 @@
 const express = require("express")
 const router = new express.Router()
 const invController = require("../controllers/invController")
+
 //Route to build inventory bt classification view
 router.get("/type/:classification_id", invController.buildByClassification);
+
+//Route to deliver a single inventory item view
+router.get("/type/detail/:inv_id", invController.buildByInvId);
 
 module.exports = router;
